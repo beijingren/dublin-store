8a0627c704530787e7f58c9536bd0afa67878791
David Höppner <0xffea@gmail.com>
2014-05-18 15:47:24 +0200

    More punctation annotations

--- scripts/annotate.rb
+++ scripts/annotate.rb
@@ -2,7 +2,7 @@
 
 ARGV.each do |filename|
   text = File.read(filename)
-  text = text.gsub(/([，。！、“”「」])/, "\n<pc>" + '\1' + "</pc>\n")
+  text = text.gsub(/([，。！、“”「」：])/, "\n<pc>" + '\1' + "</pc>\n")
   text = text.gsub(/《(.*)》/, "\n<pc>《</pc>\n<title>" + '\1' + "</title>\n<pc>》</pc>\n")
   text = text.gsub(/[△○•]/, '')
   puts text
