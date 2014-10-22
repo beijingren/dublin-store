6da95b073e0d39a65194d417d829b1db950229e9
David Höppner <0xffea@gmail.com>
2014-10-22 12:15:49 +0200

    Fix more rhymes

--- scripts/rime.py
+++ scripts/rime.py
@@ -1,10 +1,12 @@
 # coding=utf-8
 
-with open('GY_ping.txt', 'r') as f:
-    for line in f:
-        rime_line = line.decode('utf-8').rstrip().split(" ")
-        rime_head = rime_line[0]
-        for rime in rime_line:
-            print u"ClassAssertion(:Glyph :{})".format(rime)
-            print u'DataPropertyAssertion(:rime :{} "{}"^^xsd:string)'.format(rime, rime_head)
-            print u'DataPropertyAssertion(:tone :{} "平"^^xsd:string)'.format(rime)
+for (file, tone) in [('001.txt', u'平'), ('002.txt', u'上'), ('003.txt', u'去'), ('004.txt', u'入')]:
+    with open(file, 'r') as f:
+        for line in f:
+            rime_line = line.decode('utf-8').rstrip().split(" ")
+            rime_head = rime_line[0]
+            for rime in rime_line:
+                print u"ClassAssertion(:Glyph :{})".format(rime).encode('utf-8')
+                print u'DataPropertyAssertion(:PSY :{} "{}"^^xsd:string)'.format(rime, rime_head).encode('utf-8')
+                print u'DataPropertyAssertion(:tone :{} "{}"^^xsd:string)'.format(rime, tone).encode('utf-8')
+        print '\n'
